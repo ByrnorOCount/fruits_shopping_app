48ebfb42ff2c7550a13f9c3ea795a346b25a34d0
ByrnorOCount <pearcider999@gmail.com>
2026-05-20 14:17:07 +0700

    Remove unneeded lib implementations
    Signed-off-by: ByrnorOCount <pearcider999@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.mopr.fruits_app"
         minSdk = 24
-        targetSdk = 36
+        this.targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -39,11 +39,8 @@ android {
 
 dependencies {
     implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
-    implementation(libs.firebase.storage.ktx)
-    implementation(libs.glide)
 
     implementation(libs.activity.ktx)
     implementation(libs.appcompat)
